6b373cadaf4eca97b7175fa14e41b683e89f937a
Chhavi Khosla <chhavikhosla10@gmail.com>
2026-08-17 22:25:54 +0530

    Fix watsonx model configuration

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -6,16 +6,14 @@ load_dotenv()
 IBM_API_KEY = os.getenv("IBM_API_KEY", "")
 IBM_PROJECT_ID = os.getenv("IBM_PROJECT_ID", "")
 IBM_REGION = os.getenv("IBM_REGION", "jp-tok")
-
 IBM_TOKEN_URL = "https://iam.cloud.ibm.com/identity/token"
-
 IBM_MODEL_ID = os.getenv(
-"IBM_MODEL_ID",
-"mistralai/mistral-small-3-1-24b-instruct-2503"
+    "IBM_MODEL_ID",
+    "mistralai/mistral-small-3-1-24b-instruct-2503"
 )
 
 def get_watsonx_generation_url() -> str:
-return (
-f"https://{IBM_REGION}.ml.cloud.ibm.com"
-"/ml/v1/text/generation?version=2025-02-11"
-)
+    return (
+        f"https://{IBM_REGION}.ml.cloud.ibm.com"
+        "/ml/v1/text/generation?version=2025-02-11"
+    )
